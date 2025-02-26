8b66b7ada6871bf08bc07bd1111794e47cf7d3cd
shadowmourne <62612619+nagamov8584@users.noreply.github.com>
2025-02-26 10:35:52 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,11 +11,11 @@ time.tzset()
 
 progress_text = "Day in progress. Please wait."
 
-day_start = time.struct_time((2025, 2, 25, 
+day_start = time.struct_time((2025, 2, 26, 
                              9, 0, 0, 
                              3, 51, 0))
 
-day_end = time.struct_time((2025, 2, 25, 
+day_end = time.struct_time((2025, 2, 26, 
                            18, 0, 0, 
                            3, 51, 0))
 
